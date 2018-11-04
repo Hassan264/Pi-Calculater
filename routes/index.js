95156e410be2bc31c30445243d85031f86dd8c62
Hassan <hassanjobsco@gamil.com>
2018-11-04 13:22:59 +0800

    Add worker via cluster module, prints on console correct pi

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,59 @@
 var express = require('express');
 var router = express.Router();
-var math = require('mathjs').create({ number: 'BigNumber', precision: 100 });
+var math = require('mathjs');
+math.config({ number: 'BigNumber', precision: 1000 });
 
-var m1 = math.eval('sqrt(8) / 99^2');
-var next = 0;
+var cluster = require('cluster');
+
+var worker;
+
+var counter = 1;
+var pi = 0;
+
+function initWorker() {
+  worker = cluster.fork();
+  console.log('worker is: ' + worker);
+  worker.on('message', function (msg) {
+    console.log("new PI: " + msg);
+    console.log(msg);
+    pi = msg.pi;
+    sendCounter();
+  })
+  sendCounter(); // kickoff initial calculation
+}
+
+function sendCounter() {
+  worker.send({ n: counter });
+  counter++;
+}
+
+if (cluster.isMaster) {
+  console.log('init worker');
+  initWorker();
+}
+else {
+  var worker = require('cluster').worker;
+  console.log('worker code: ' + worker);
+  worker.on('message', function (msg) {
+    console.log('in on message worker: ' + msg);
+    var result = 0;
+    var m1 = math.eval('sqrt(8) / 99^2');
+
+    for (var i = 0; i < msg.n; i++) {
+      var newResult = math.eval('((4 * n)! * (1103 + 26390 * n)) / ((n!)^4 * 396^(4*n))', { n: math.bignumber(i) });
+      result = math.eval('r1 + r2', { r1: math.bignumber(result), r2: math.bignumber(newResult) });
+    }
+    var pi = math.eval('1 / (m1 * r)', { m1: math.bignumber(m1), r: math.bignumber(result) });
+    process.send({ pi: pi });
+
+  });
+}
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var result = 0;
-
-
-  for (var i = 0; i < 800; i++) {
-    var newResult = math.eval('((4 * n)! * (1103 + 26390 * n)) / ((n!)^4 * 396^(4*n))', { n: math.bignumber(i) });
-    result = math.eval('r1 + r2', { r1: math.bignumber(result), r2: math.bignumber(newResult) });
-  }
-  var pi = math.eval('1 / (m1 * r)', { m1: math.bignumber(m1), r: math.bignumber(result) });
 
   res.send({
-    result: pi.toString()
+    result: pi
   });
 });
 
