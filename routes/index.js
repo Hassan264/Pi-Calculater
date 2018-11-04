b1df5ac75efab1b037ce5eb682892ee15f95d681
Hassan <hassanjobsco@gamil.com>
2018-11-04 12:25:07 +0800

    Adds dependency and calculate pi using Ramanujan’s Formula for Pi

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var math = require('mathjs').create({ number: 'BigNumber', precision: 100 });
+
+var m1 = math.eval('sqrt(8) / 99^2');
+var next = 0;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  var result = 0;
+
+
+  for (var i = 0; i < 800; i++) {
+    var newResult = math.eval('((4 * n)! * (1103 + 26390 * n)) / ((n!)^4 * 396^(4*n))', { n: math.bignumber(i) });
+    result = math.eval('r1 + r2', { r1: math.bignumber(result), r2: math.bignumber(newResult) });
+  }
+  var pi = math.eval('1 / (m1 * r)', { m1: math.bignumber(m1), r: math.bignumber(result) });
+
+  res.send({
+    result: pi.toString()
+  });
 });
 
 module.exports = router;
